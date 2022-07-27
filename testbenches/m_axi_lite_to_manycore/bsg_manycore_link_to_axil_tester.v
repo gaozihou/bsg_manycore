8c3d5360f55984a26e94a8425069afd06eafb870
Paul Gao <gaozihou@uw.edu>
2022-07-26 21:43:22 -0700

    improve axil tester

--- testbenches/m_axi_lite_to_manycore/bsg_manycore_link_to_axil_tester.v
+++ testbenches/m_axi_lite_to_manycore/bsg_manycore_link_to_axil_tester.v
@@ -21,7 +21,7 @@ module bsg_manycore_link_to_axil_tester
   ,input  [ 1:0]                       io_axi_lite_bresp
   ,input                               io_axi_lite_bvalid
   ,output                              io_axi_lite_bready
-  ,output [ axil_addr_width_p-1:0]     io_axi_lite_araddr
+  ,output logic [ axil_addr_width_p-1:0] io_axi_lite_araddr
   ,output logic                        io_axi_lite_arvalid
   ,input                               io_axi_lite_arready
   ,input  [ axil_data_width_p-1:0]     io_axi_lite_rdata
@@ -36,15 +36,23 @@ module bsg_manycore_link_to_axil_tester
     logic [31:0] data;
   } bsg_nbf_s;
 
+  typedef enum logic [3:0] {
+    LOAD_NBF
+   ,AR_OCCUPACY
+   ,R_OCCUPACY
+   ,AR_MC_REQ
+   ,R_MC_REQ
+  } nbf_state_e;
+
   logic [67:0] nbf [max_nbf_p-1:0];
   logic [nbf_addr_width_lp-1:0] nbf_addr_r, nbf_addr_n;
-  logic [7:0] nbf_counter_r, nbf_counter_n;
+  logic [31:0] nbf_counter_r, nbf_counter_n;
+  nbf_state_e nbf_state_r, nbf_state_n;
 
   bsg_nbf_s curr_nbf;
   assign curr_nbf = nbf[nbf_addr_r];
 
   assign io_axi_lite_awaddr = curr_nbf.addr;
-  assign io_axi_lite_araddr = curr_nbf.addr;
   assign io_axi_lite_wdata  = curr_nbf.data;
   assign io_axi_lite_wstrb  = '1;
   assign io_axi_lite_bready = 1'b1;
@@ -61,29 +69,76 @@ module bsg_manycore_link_to_axil_tester
   always_comb
   begin
     nbf_addr_n = nbf_addr_r;
-    nbf_counter_n = nbf_counter_r + pcie_en_i;
+    nbf_counter_n = nbf_counter_r;
+    nbf_state_n = nbf_state_r;
     io_axi_lite_arvalid = 1'b0;
     io_axi_lite_awvalid = 1'b0;
     io_axi_lite_wvalid  = 1'b0;
-    if (nbf_counter_r == 127)
+    io_axi_lite_araddr  = curr_nbf.addr;
+    if (nbf_state_r == LOAD_NBF)
       begin
-        nbf_counter_n = '0;
-        if (is_finish)
-          begin 
-            // do nothing
+        nbf_counter_n = nbf_counter_r + pcie_en_i;
+        if (nbf_counter_r == 127)
+          begin
+            nbf_counter_n = 0;
+            if (is_finish)
+              begin
+                nbf_state_n = AR_OCCUPACY;
+              end
+            else if (is_read)
+              begin
+                io_axi_lite_arvalid = 1'b1;
+                if (io_axi_lite_arready)
+                    nbf_addr_n = nbf_addr_r + 1;
+              end
+            else if (is_write)
+              begin
+                io_axi_lite_awvalid = 1'b1;
+                io_axi_lite_wvalid = 1'b1;
+                if (io_axi_lite_awready & io_axi_lite_wready)
+                    nbf_addr_n = nbf_addr_r + 1;
+              end
           end
-        else if (is_read)
+      end
+    else if (nbf_state_r == AR_OCCUPACY)
+      begin
+        io_axi_lite_arvalid = 1'b1;
+        io_axi_lite_araddr  = 16'h1018;
+        if (io_axi_lite_arready)
+            nbf_state_n = R_OCCUPACY;
+      end
+    else if (nbf_state_r == R_OCCUPACY)
+      begin
+        if (io_axi_lite_rvalid)
+          begin
+            nbf_counter_n = io_axi_lite_rdata;
+            nbf_state_n = AR_MC_REQ;
+          end
+      end
+    else if (nbf_state_r == AR_MC_REQ)
+      begin
+        if (nbf_counter_r == 0)
+          begin
+            nbf_state_n = AR_OCCUPACY;
+          end
+        else
           begin
             io_axi_lite_arvalid = 1'b1;
+            io_axi_lite_araddr  = 16'h101C;
             if (io_axi_lite_arready)
-                nbf_addr_n = nbf_addr_r + 1;
+                nbf_state_n = R_MC_REQ;
           end
-        else if (is_write)
+      end
+    else if (nbf_state_r == R_MC_REQ)
+      begin
+        if (io_axi_lite_rvalid)
           begin
-            io_axi_lite_awvalid = 1'b1;
-            io_axi_lite_wvalid = 1'b1;
-            if (io_axi_lite_awready & io_axi_lite_wready)
-                nbf_addr_n = nbf_addr_r + 1;
+            nbf_counter_n = nbf_counter_r - 1;
+            nbf_state_n = AR_MC_REQ;
+            if (io_axi_lite_rdata[16+:16] == 16'h3AB4)
+              begin
+                $finish;
+              end
           end
       end
   end
@@ -94,11 +149,13 @@ module bsg_manycore_link_to_axil_tester
       begin
         nbf_addr_r <= '0;
         nbf_counter_r <= '0;
+        nbf_state_r <= LOAD_NBF;
       end
     else
       begin
         nbf_addr_r <= nbf_addr_n;
         nbf_counter_r <= nbf_counter_n;
+        nbf_state_r <= nbf_state_n;
       end
   end
 
